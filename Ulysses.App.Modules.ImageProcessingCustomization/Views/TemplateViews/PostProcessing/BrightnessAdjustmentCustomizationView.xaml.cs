e72f6b989e8d723b3a684b9067b997a5dd67895c
rafkos <poczta.raf.kos@gmail.com>
2016-01-28 20:47:15 +0100

    Minor View fixes

--- Ulysses.App.Modules.ImageProcessingCustomization/Views/TemplateViews/PostProcessing/BrightnessAdjustmentCustomizationView.xaml.cs
+++ Ulysses.App.Modules.ImageProcessingCustomization/Views/TemplateViews/PostProcessing/BrightnessAdjustmentCustomizationView.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Ulysses.App.Modules.ImageProcessingCustomization.ViewModels.Templates.PostProcessing;
 
 namespace Ulysses.App.Modules.ImageProcessingCustomization.Views.TemplateViews.PostProcessing
 {
-    /// <summary>
-    /// Interaction logic for BrightnessAdjustmentCustomizationView.xaml
-    /// </summary>
-    public partial class BrightnessAdjustmentCustomizationView : UserControl
+    public partial class BrightnessAdjustmentCustomizationView
     {
-        public BrightnessAdjustmentCustomizationView()
+        public BrightnessAdjustmentCustomizationView(IBrightnessAdjustmentCustomizationViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
--- Ulysses.App.Modules.ImageProcessingCustomization/Views/TemplateViews/PostProcessing/ContrastAdjustmentCustomizationView.xaml.cs
+++ Ulysses.App.Modules.ImageProcessingCustomization/Views/TemplateViews/PostProcessing/ContrastAdjustmentCustomizationView.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Ulysses.App.Modules.ImageProcessingCustomization.ViewModels.Templates.PostProcessing;
 
 namespace Ulysses.App.Modules.ImageProcessingCustomization.Views.TemplateViews.PostProcessing
 {
-    /// <summary>
-    /// Interaction logic for ContrastAdjustmentCustomizationView.xaml
-    /// </summary>
-    public partial class ContrastAdjustmentCustomizationView : UserControl
+    public partial class ContrastAdjustmentCustomizationView
     {
-        public ContrastAdjustmentCustomizationView()
+        public ContrastAdjustmentCustomizationView(IContrastAdjustmentCustomizationViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
